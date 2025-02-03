2898464797f9e977aeac6472b3e4e8a21879175d
jitendra singh <jitendra.maxxxx@gmail.com>
2025-02-03 11:08:33 +0530

    first commit

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express from 'express';
 import cors from 'cors';
 
 const app=express();
-app.use(cors);
+app.use(cors([]));
 
 app.get('/',(res,resp)=>{
     resp.send("this is http surver from jitendra singh");
 })
 
-app.listen(8080)
+app.listen(4000,()=>console.log("started"))
